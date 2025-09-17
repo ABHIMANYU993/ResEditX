793bf891805f615a73ff2d2b07796745fc6fce65
ABHIMANYU993 <abhimanyubadiger1001@gmail.com>
2025-09-17 15:42:01 +0530

    Expand user schema for subscriptions

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,8 +3,10 @@ const { usersDB } = require("../database/connection");
 
 // User Schema
 const usersSchema = new mongoose.Schema({
+  is_subscribed:{type:Boolean,require:false},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  credits:{type:Number,require:false}
 });
 
 module.exports = usersDB.model("users", usersSchema);
